d1e23e414efbe3bd64726fb17037e36ad6f570c7
PROPERTY\214843 <mintumia8@gmail.com>
2023-10-22 19:32:50 +0600

    Navigation Menu Changed

---
+++ src/components/testing.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<h1 class="bg-red-600">Tesing Page</h1>
+</template>
+
+<style scoped>
+
+</style>
--- src/layouts/appLayout.vue
+++ src/layouts/appLayout.vue
@@ -1,13 +1,17 @@
 <script setup lang="js">
+import {ref} from 'vue';
 import { RouterView } from 'vue-router';
 import FooterItems from "@/components/footerItems.vue";
 import HeaderItems from "@/components/headerItems.vue";
 import NavBar from "@/components/navBar.vue";
 import NewNavBar from "@/components/NewNavBar.vue";
 import NewNavBar02 from "@/components/NewNavBar02.vue";
+import Testing from "@/components/testing.vue";
 /*import PlaceHolderImage from "@/components/images/placeHolderImage.vue";
 import PlaceHolderImage01 from "@/components/images/placeHolderImage01.vue";
 import PlaceHolderImage02 from "@/components/images/placeHolderImage02.vue";*/
+const testing = ref(null);
+
 </script>
 
 <template>
@@ -17,9 +21,16 @@ import PlaceHolderImage02 from "@/components/images/placeHolderImage02.vue";*/
 
     <header-items/>
    <main>
-     <transition>
+
+     <router-view v-slot="{ Component }">
+       <transition>
+         <component :is="Component" />
+       </transition>
+     </router-view>
+
+<!--     <transition>
        <router-view/>
-     </transition>
+     </transition>-->
 
    </main>
 
