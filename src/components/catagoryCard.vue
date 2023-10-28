1f1c38daf5923209a00c426839cdeb2754fc4f5d
PROPERTY\214843 <mintumia8@gmail.com>
2023-10-28 20:23:42 +0600

    Catagory Card Modified

--- src/components/catagoryCard.vue
+++ src/components/catagoryCard.vue
@@ -1,20 +1,53 @@
-<script setup lang="ts">
+<script setup>
+import {ref,defineProps} from 'vue';
+
+const isVisible =ref(false);
+const showFull = ref(false);
+const isOver = ref(false);
+const myProps = defineProps({
+  postTitle:String,
+  description:String,
+  imagePath:String,
+
+});
+
+function  countChar(limit = 20){
+  if(myProps.description.length >limit && showFull.value===false){
+    //console.log("limit : "+limit);
+    isOver.value = true;
+    return myProps.description.slice(0,limit)+"...";
+  }else {
+    return myProps.description;
+  }
+
+}
 
 </script>
 
+
 <template>
-<div class="mx-auto">
+  <div class="relative w-[320px] min-h-[230px] m-3 p-2 rounded-lg shadow-lg border-2 bg-white z-10 ">
 
-   <div class="relative mx-3 my-1 z-10" >
+    <router-link to="/about" @mouseover="isVisible = true" @mouseout="isVisible = false" >
 
-      <div class="absolute bottom-3 text-center text-gray-200 z-30 opacity-90 bg-indigo-700"><h3 class="text-xl">Card Title and</h3> </div>
-    </div>
-     <Icon class="w-48 h-40 bg-white text-gray-400 rounded-lg border-2 drop-shadow-lg shadow-black" name="bi:card-image"/>
-
-
-</div>
+      <div>
+        <span :class='isVisible ? "block":"hidden"' class="absolute opacity-25 -left-0 -top-0 min-w-full min-h-full bg-gray-300 z-20"></span>
+        <div class="flex content-center justify-center p-2 mx-auto">
+          <!--            <img :src="imagePath" :alt="postTitle">-->
+          <img :src="imagePath" :alt="postTitle">
+        </div>
+        <div class="text-2xl font-medium text-black text-center">
+          <h1>{{postTitle}}</h1>
+        </div>
+        <div class="relative p-2 text-center">
+          <p>
+            {{countChar(100)}} <button v-if="isOver" class="bg-blue-300 rounded-md shadow-lg border-2 border-gray-500 p-1" @click="()=>showFull = !showFull"><span v-if="showFull===false">Read More</span><span v-if="showFull===true">Read Less</span></button>
+          </p>
+        </div>
+      </div>
+    </router-link>
+  </div>
 </template>
 
-<style scoped>
 
-</style>
+
